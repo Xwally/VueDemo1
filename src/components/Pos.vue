1a193d3fabd3b8765d23f16eee6a0cf3a096553f
maobao.tyc <taoyunchuan@mobilekm.cn>
2019-05-06 21:12:07 +0800

    Initial commit

---
+++ src/components/Pos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pos">
+    Hello AwesomePos Demo.
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Pos"
+    }
+</script>
+
+<!--scoped 代表局部变量-->
+<style scoped>
+
+</style>
